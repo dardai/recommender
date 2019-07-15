b2ebd44c06007e64b20af5438a5baa16e4e9e108
hanjiadi <380351468@qq.com>
2019-07-15 19:00:12 +0800

    7.12韩佳迪-用户课程和用户考试的模拟数据降维

--- recSys/views.py
+++ recSys/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core import serializers
+from django.http import JsonResponse
 
 # Create your views here.
+def test_index(request):
+    blogs = '欢迎来到智能推荐系统'
+    print (request.GET)
+    #if 'q' in request.GET:
+    #    message = 'search ' + request.GET['q']
+    #else:
+    #    message = '空表单'
+    return JsonResponse({'res':1,'name':'hhhhhh'},json_dumps_params={'ensure_ascii':False})
+    #return HttpResponse(message)
+   # return  render(request,"recommend/testIndex.html",{"rec":blogs})
